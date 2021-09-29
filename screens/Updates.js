234cbfa8dbb090123b344e4e0e2520e467be8090
Atharv-prog <72858850+Atharv-prog@users.noreply.github.com>
2021-09-29 14:28:53 +0530

    Add files via upload

---
+++ screens/Updates.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View,  } from 'react-native';
+
+export default class UpdateScreen extends Component {
+   render(){
+       return(
+       <View style={{flex:1, justifycontent:"center",alignItems:"center"}}>
+        <Text>Update screen</Text>      
+       </View>
+
+       )
+   }
+}
